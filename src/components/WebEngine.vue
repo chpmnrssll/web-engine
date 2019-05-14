8c27470c1627141c776efa9a61d8bccfd482d9b0
Russell <chpmnrssll@gmail.com>
2019-05-13 21:06:58 -0600

    Added mini map

--- src/components/WebEngine.vue
+++ src/components/WebEngine.vue
@@ -2,7 +2,17 @@
 <template>
   <div class="webEngineContainer">
     <canvas ref="canvas3D" />
-    <!-- <canvas ref="canvas2D" /> -->
+    <canvas
+      id="canvas2D"
+      ref="canvas2D"
+    />
+    <section>
+      <ul>
+        <li>Left click + drag = move</li>
+        <li>Right click + drag = rotate</li>
+        <li>Scroll = zoom</li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -10,128 +20,148 @@
 import * as THREE from 'three';
 import NoiseMap from 'noise-map';
 import { QuadTree, isoLines } from 'marchingsquares';
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { MapControls } from 'three/examples/jsm/controls/MapControls';
 
 export default {
   name: 'WebEngine',
   mounted() {
-    // this.canvas2D = this.$refs.canvas2D;
-    // this.context2D = this.canvas2D.getContext('2d');
+    this.canvas2D = this.$refs.canvas2D;
+    this.context2D = this.canvas2D.getContext('2d');
 
     this.canvas3D = this.$refs.canvas3D;
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas3D });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    this.generator = new NoiseMap.MapGenerator();
-    this.noiseMap = this.generator.createMap(400, 200, {
-      type: 'simplex',
-      amplitude: 1,
-      frequency: 0.5,
-      amplitudeCoef: 0.5,
-      frequencyCoef: 0.5,
-      elevation: 0.5,
-      step: false,
-      stepValue: 30,
-    });
-
-    // this.noiseMap.stepValues(30);
-
-    // draw the heights in B&W
-    // this.noiseMap.draw(
-    //   this.context2D,
-    //   this.canvas2D.width,
-    //   this.canvas2D.height,
-    //   NoiseMap.STYLE.GRAY,
-    // );
-    const data = [];
-
-    for (let y = 0; y < this.noiseMap.height; y += 1) {
-      const row = [];
-      for (let x = 0; x < this.noiseMap.width; x += 1) {
-        row.push(this.noiseMap.get(x, y) * 255);
-      }
-      data.push(row);
-    }
-
-    const prepData = new QuadTree(data);
-    const contours = isoLines(prepData, [160, 128, 96, 64], {
-      // successCallback: () => {},
-      // verbose: true,
-      // polygons: true,
-      // linearRing: true,
-      // noQuadTree: false,
-      // noFrame: false,
-    });
-
-    this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 1000,
     );
-
-    contours.forEach((contour, contourNumber) => {
-      const r = parseInt(64 - (contourNumber * 8), 10);
-      const g = parseInt(128 + (contourNumber * 8), 10);
-      const b = parseInt(192 - (contourNumber * 8), 10);
-      contour.forEach((path) => {
-        const pathShape = new THREE.Shape();
-
-        // this.context2D.save();
-        // this.context2D.beginPath();
-        // this.context2D.strokeStyle = `rgb(${r},${g},${b})`;
-        path.forEach((vertex, index) => {
-          pathShape.moveTo(vertex[0], vertex[1]);
-          // this.context2D.moveTo(vertex[0], vertex[1]);
-          const nextVertex = path[index + 1];
-          if (nextVertex) {
-            pathShape.lineTo(nextVertex[0], nextVertex[1]);
-            // this.context2D.lineTo(nextVertex[0], nextVertex[1]);
-          }
-        });
-        // this.context2D.stroke();
-        // this.context2D.restore();
-
-        const depth = 5;
-        const geometry = new THREE.ExtrudeGeometry(pathShape, {
-          depth,
-          bevelEnabled: true,
-          bevelSegments: 2,
-          bevelSize: 2,
-          bevelThickness: 1,
-          steps: 2,
-        });
-
-        geometry.translate(-200, -100, contourNumber * depth);
-
-        const material = new THREE.MeshPhongMaterial({ color: `rgb(${r},${g},${b})` });
-        const mesh = new THREE.Mesh(geometry, material);
-        this.scene.add(mesh);
-      });
-    });
-
     this.camera.position.set(0, 0, 200);
-
-    // this.controls = new OrbitControls(this.camera);
     this.controls = new MapControls(this.camera);
 
-    this.scene.add(new THREE.AmbientLight(0x0a0a0a));
+    this.scene = new THREE.Scene();
+    this.scene.add(new THREE.AmbientLight(0x888888, 0.15));
 
-    const topLight = new THREE.PointLight(0xffeedd);
-    topLight.position.set(0, 100, 20);
+    const topLight = new THREE.PointLight(0xffeedd, 1, 512, 2);
+    topLight.position.set(0, 128, 32);
     this.scene.add(topLight);
 
-    const bottomLight = new THREE.PointLight(0xff2200);
-    bottomLight.position.set(0, -100, 10);
+    const bottomLight = new THREE.PointLight(0xff2200, 1, 1024, 1);
+    bottomLight.position.set(0, -128, 32);
     this.scene.add(bottomLight);
 
-    const animate = () => {
-      this.controls.update();
-      this.renderer.render(this.scene, this.camera);
-      window.requestAnimationFrame(animate);
-    };
+    this.generateMap();
 
     this.controls.update();
-    animate();
+    this.animate();
+  },
+  methods: {
+    generateMap() {
+      this.mapGenerator = new NoiseMap.MapGenerator();
+      this.noiseMap = this.mapGenerator.createMap(400, 200, {
+        type: 'simplex',
+        amplitude: 1,
+        frequency: 0.5,
+        amplitudeCoef: 0.5,
+        frequencyCoef: 0.5,
+        elevation: 0.5,
+        step: false,
+        stepValue: 30,
+      });
+
+      // this.context2D.filter = 'invert(100%)';
+      // draw the heightMap on context2D
+      this.noiseMap.draw(
+        this.context2D,
+        this.canvas2D.width,
+        this.canvas2D.height,
+        NoiseMap.STYLE.GRAY,
+      );
+      // this.context2D.filter = 'none';
+
+      const quadTree = this.convertNoiseMapToQuadTree();
+      const contours = isoLines(quadTree, [160, 128, 96, 64], {
+        // successCallback: () => {},
+        // verbose: true,
+        // polygons: true,
+        // linearRing: true,
+        // noQuadTree: false,
+        // noFrame: false,
+      });
+
+      contours.forEach((contour, contourNumber) => {
+        const r = parseInt(64 - (contourNumber * 8), 10);
+        const g = parseInt(128 + (contourNumber * 8), 10);
+        const b = parseInt(192 - (contourNumber * 8), 10);
+
+        contour.forEach((path) => {
+          const pathShape = this.convertPathToShape(path);
+          this.drawPathOnContext2D(path, r, g, b);
+
+          const depth = 5;
+          const geometry = new THREE.ExtrudeGeometry(pathShape, {
+            depth,
+            bevelEnabled: true,
+            bevelSegments: 2,
+            bevelSize: 2,
+            bevelThickness: 1,
+            steps: 2,
+          });
+
+          // center on x & y, z increases with each layer
+          geometry.translate(-200, 100, contourNumber * depth);
+
+          const material = new THREE.MeshPhongMaterial({ color: `rgb(${r},${g},${b})` });
+          const mesh = new THREE.Mesh(geometry, material);
+          this.scene.add(mesh);
+        });
+      });
+    },
+    animate() {
+      this.controls.update();
+      this.renderer.render(this.scene, this.camera);
+      window.requestAnimationFrame(this.animate.bind(this));
+    },
+    convertNoiseMapToQuadTree() {
+      const data = [];
+      for (let y = 0; y < this.noiseMap.height; y += 1) {
+        const row = [];
+        for (let x = 0; x < this.noiseMap.width; x += 1) {
+          row.push(this.noiseMap.get(x, y) * 255);
+        }
+        data.push(row);
+      }
+
+      return new QuadTree(data);
+    },
+    convertPathToShape(path) {
+      const pathShape = new THREE.Shape();
+
+      path.forEach((vertex, index) => {
+        pathShape.moveTo(vertex[0], -vertex[1]);
+        const nextVertex = path[index + 1];
+        if (nextVertex) {
+          pathShape.lineTo(nextVertex[0], -nextVertex[1]);
+        }
+      });
+
+      return pathShape;
+    },
+    drawPathOnContext2D(path, r, g, b) {
+      this.context2D.save();
+      this.context2D.beginPath();
+      this.context2D.strokeStyle = `rgb(${r},${g},${b})`;
+
+      path.forEach((vertex, index) => {
+        this.context2D.moveTo(vertex[0], vertex[1]);
+        const nextVertex = path[index + 1];
+        if (nextVertex) {
+          this.context2D.lineTo(nextVertex[0], nextVertex[1]);
+        }
+      });
+      this.context2D.closePath();
+      this.context2D.stroke();
+      this.context2D.restore();
+    },
   },
 };
 </script>
@@ -141,5 +171,21 @@ export default {
 canvas {
   height: 100vh;
   width: 100vw;
+}
+#canvas2D {
+  bottom: 1rem;
+  height: 33vh;
+  opacity: 0.75;
+  position: absolute;
+  right: 1rem;
+  width: 33vw;
+}
+section {
+  bottom: 1rem;
+  color: white;
+  font-size: 0.5rem;
+  left: 1rem;
+  list-style-type: none;
+  position: absolute;
 }
 </style>
